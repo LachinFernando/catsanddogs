dff894ee1f761446a09dc0fc2f1253eedcc1af9f
LachinFernando <lachin.fernando@pyxeda.ai>
2023-11-06 06:47:47 +0530

    add(script extractor)

--- test.py
+++ test.py
@@ -41,3 +41,63 @@ def get_video_id_from_url(url):
     
     video_id= url_chunks.query
     return video_id.split("=")[-1]
+
+def process_get_request(event: dict) -> dict:
+
+    params = event['queryStringParameters']
+    summary_id = req_utils.get_params(params, 'summaryId')
+    youtube_link = req_utils.get_params(params, 'youtubeLink')
+    extract_type = req_utils.get_params(params, 'extractType')
+    session_id = req_utils.get_params(params, 'sessionId')
+    previous_session_summaries = req_utils.get_params(
+        params, "previousSessionSummaries"
+    )
+    video_id = get_video_id_from_url(youtube_link)
+    print("Video ID", video_id)
+
+    # get the processed transcript from the youtube videeo
+    try:
+        full_converation, ten_min_conversation = process_youtube_script(video_id)
+        print("characters in full conversation: ", len(full_converation))
+    except TranscriptsDisabled as error:
+        message = f"Transcripts disabled: {error}"
+        print(message)
+        record_summary_result(summary_id, "failure", summary=message, homework="")
+        return None
+    except Exception as error:
+        message = f"Unable to get the content from the youtube video err: {error}"
+        print(message)
+        record_summary_result(summary_id, "failure", summary=message, homework="")
+        return None
+
+    # first try to get the summary assuming tokens are not exceeding the limit
+    try:
+        summary, homework = get_summary(full_converation, ten_min_conversation)
+        print("final summary: ", summary)
+        print("homework: ", homework)
+        record_summary_result(summary_id, "success", summary, homework)
+        if extract_type == "auto":
+            previous_session_summaries.append(summary)
+            record_summary_to_project_session(session_id, previous_session_summaries)
+            record_homework(homework, session_id)
+    except Exception as error:
+        print("Error message for the first summary attempt: {}".format(error))
+        # it may fail due to large number of tokens
+        # try again to get the summary by taking chunks
+        try:
+            split_ratio = int(np.ceil(len(full_converation) / MAX_CHARACTERS_ALLOWED))
+            print("split ratio", split_ratio)
+            summary, homework = get_summary(
+                full_converation, ten_min_conversation, split_ratio
+            )
+            print("final summary: ", summary)
+            print("homework: ", homework)
+            record_summary_result(summary_id, "success", summary, homework)
+            if extract_type == "auto":
+                previous_session_summaries.append(summary)
+                record_summary_to_project_session(session_id, previous_session_summaries)
+                record_homework(homework, session_id)
+        except Exception as error:
+            message = f"Unable to get the summary for the transcript err: {str(error)}"
+            print(message)
+            record_summary_result(summary_id, "failure", "", "")
